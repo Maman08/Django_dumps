2cd392e9f5787ed62884afb155385a8bfd12434a
Maman08 <a34mritunjaysingh@gmail.com>
2025-02-25 12:34:56 +0530

    added new api

--- taskmanager/taskmanager/tasks/views.py
+++ taskmanager/taskmanager/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
+from django.contrib.auth.models import User
+from .serializers import UserSerializers
+from rest_framework import generics, permissions
 # Create your views here.
+
+
+class UserCreate(generics.CreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserSerializers
+    permissions_classes=[permissions.AllowAny]
+
