21b05bafc8282f2328e6cf7bc85dbe03dc5ba2d2
Maman08 <a34mritunjaysingh@gmail.com>
2025-03-06 22:24:59 +0530

    view

--- todo/todoapplication/accounts/serializers.py
+++ todo/todoapplication/accounts/serializers.py
@@ -10,3 +10,17 @@ class UserSerialezer(serializers.ModelSerializer):
     def create(self , validated_data):
         user=User.objects.create_user(**validated_data)
         return user    
+    
+
+
+
+
+
+
+# Serializers ka use Django models ko JSON format me convert karne ke liye hota hai, taaki data APIs me send/receive kiya ja sake.    
+# serializers.ModelSerializer → Ye Django model ke fields ko automatically serialize kar deta hai.
+
+# User.objects.create_user(**validated_data)
+
+#     Django ka built-in create_user method ka use ho raha hai jo password ko hash (encrypt) kar deta hai.
+#     Agar hum User.objects.create(**validated_data) use karein, to password plaintext (unencrypted) store hoga, jo security issue hai.
--- todo/todoapplication/accounts/views.py
+++ todo/todoapplication/accounts/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class RegisterView(CreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    permission_classes=AllowAny
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+
+# Create your views here.permission_classes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # djoser.urls includes:
+    #     /auth/users/ → Get all users.
+    #     /auth/users/me/ → Get the currently logged-in user.
+    #     /auth/users/confirm/ → Confirm email (if configured).
+    #     /auth/users/reset_password/ → Reset password.
+    # djoser.urls.authtoken includes:
+    #     /auth/token/login/ → Login and get an authentication token.
+    #     /auth/token/logout/ → Logout (delete the token).
+
